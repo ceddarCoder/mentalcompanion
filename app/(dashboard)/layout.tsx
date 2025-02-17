6944b50d6349ad259606de8c6bc6dbd164ddcca7
saketh <wow04wow@gmail.com>
2025-02-17 16:06:45 +0530

    added vercel analytics

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,11 @@
 import DashboardLayout from "@/components/dashboard/DashboardLayout";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <DashboardLayout>{children}</DashboardLayout>;
+  return (
+  <div>
+    <Analytics/><DashboardLayout>{children}</DashboardLayout>
+  </div>
+  
+  );
 }
